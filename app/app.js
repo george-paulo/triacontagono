180ab864b126466e474404f37c388cf120cc6343
George Paulo <122838710+george-paulo@users.noreply.github.com>
2023-10-14 18:32:40 -0300

    Update app.js
    Colocando explicação do problema.

--- app/app.js
+++ app/app.js
@@ -55,7 +55,18 @@ function cercaQ(req, res, query) {
     const nome = query.nome;
     const tipo = query.tipo;
     const lado = parseFloat(query.lado);
+
+    // Calcula a área
     const area = (30 * lado * lado) / (4 * (1 / Math.tan(Math.PI / 30)));
+    
+    // Define a mensagem de acordo com o tamanho da cerca
+    let mensagem = "";
+    if (area > 200) {
+        mensagem = "É uma cerca grande.";
+    } else {
+        mensagem = "É uma cerca pequena.";
+    }
+    
     const motivo = "Calcule a área de uma cerca em forma de triacontágono. Se a área for maior que 200 metros quadrados, é uma cerca grande. Se for menor que 200 metros quadrados, é uma cerca pequena.";
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -68,10 +79,11 @@ function cercaQ(req, res, query) {
     res.write(`<h1>Problema: Calcular Área de Cerca</h1>`);
     res.write(`<p>Nome: ${nome}</p>`);
     res.write(`<p>Tipo: ${tipo}</p>`);
-    res.write(`<p>Lado: ${lado}</p>`);
+    res.write(`<p>Lado: ${lado} metros</p>`);
     res.write(`<p>Área: ${area} metros quadrados</p>`);
     res.write(`<p>Explicação da conta:</p>`);
     res.write(`<p>${motivo}</p>`);
+    res.write(`<p>${mensagem}</p>`; // Exibe se é uma cerca grande ou pequena
     res.write('<footer>');
     res.write('<p>Desenvolvido Por George Paulo</p>');
     res.write('</footer>');
@@ -92,13 +104,15 @@ function autor(req, res) {
     res.write('<p>Nome: George Paulo</p>');
     res.write('<h2>Formações acadêmicas:</h2>');
     res.write('<ul>');
-    res.write('<li>Técnico em Informártica para Internet</li>');
-    res.write('<li>Instituo Federal do Ceará</li>');
+    res.write('<li>Técnico em Informática para Internet</li>');
+    res.write('<li>Instituto Federal do Ceará</li>');
+    res.write('<li>Ano: 2023</li>');
     res.write('</ul>');
     res.write('<h2>Experiências profissionais:</h2>');
     res.write('<ul>');
     res.write('<li>Militar do Exército Brasileiro</li>');
-    res.write('<li>Trabalhando atualmente com infraestruturada de TI</li>');
+    res.write('<li>Trabalhando atualmente com infraestrutura de TI</li>');
+    res.write('<li>Ano: Desde 2018</li>');
     res.write('</ul>');
     res.write('<footer>');
     res.write('<p>Desenvolvido Por George Paulo</p>');
