5da7135f955af1e593c580c20d68d8a2ab40b284
George Paulo <122838710+george-paulo@users.noreply.github.com>
2023-10-15 11:31:49 -0300

    Update app.js
    Atualizando linguagem

--- app/app.js
+++ app/app.js
@@ -25,9 +25,9 @@ function index(req, res) {
     <meta charset="UTF-8">
     </head>
     <body>`);
-    res.write('<h1>Problema: Calcular Ãrea de Cerca Q</h1>');
-    res.write('<p>DescriÃ§Ã£o do problema:</p>');
-    res.write('<p>Calcule a Ã¡rea de uma cerca em forma de triacontÃ¡gono. Se a Ã¡rea for maior que 200 metros quadrados, Ã© uma cerca grande. Se for menor que 200 metros quadrados, Ã© uma cerca pequena.</p>');
+    res.write('<h1>Problema: Calcular Área de Cerca Q</h1>');
+    res.write('<p>Descrição do problema:</p>');
+    res.write('<p>Calcule a Área de uma cerca em forma de triacontagono. Se a Área for maior que 200 metros quadrados, É uma cerca grande. Se for menor que 200 metros quadrados, É uma cerca pequena.</p>');
     res.write('<form action="/triacontagono" method="get">');
     res.write('<label>');
     res.write('<span>Nome</span>');
@@ -60,12 +60,12 @@ function cercaQ(req, res, query) {
     
     let mensagem = "";
     if (area > 200) {
-        mensagem = "Ã‰ uma cerca grande.";
+        mensagem = "É uma cerca grande.";
     } else {
-        mensagem = "Ã‰ uma cerca pequena.";
+        mensagem = "É uma cerca pequena.";
     }
     
-    const motivo = "Calcule a Ã¡rea de uma cerca em forma de triacontÃ¡gono. Se a Ã¡rea for maior que 200 metros quadrados, Ã© uma cerca grande. Se for menor que 200 metros quadrados, Ã© uma cerca pequena.";
+    const motivo = "Calcule a Área de uma cerca em forma de triacontagono. Se a Área for maior que 200 metros quadrados, É uma cerca grande. Se for menor que 200 metros quadrados, É uma cerca pequena.";
 
    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(`<!DOCTYPE html>
@@ -74,12 +74,12 @@ function cercaQ(req, res, query) {
     <meta charset="UTF-8">
     </head>    
     <body>`);
-    res.write(`<h1>Problema: Calcular Ãrea de Cerca</h1>`);
+    res.write(`<h1>Problema: Calcular Área de Cerca</h1>`);
     res.write(`<p>Nome: ${nome}</p>`);
     res.write(`<p>Tipo: ${tipo}</p>`);
     res.write(`<p>Lado: ${lado} metros</p>`);
-    res.write(`<p>Ãrea: ${area} metros quadrados</p>`);
-    res.write(`<p>ExplicaÃ§Ã£o da conta:</p>`);
+    res.write(`<p>Área: ${area} metros quadrados</p>`);
+    res.write(`<p>Explicação da conta:</p>`);
     res.write(`<p>${motivo}</p>`);
     res.write(`<p>${mensagem}</p>`);
     res.write('<footer>');
@@ -100,15 +100,15 @@ function autor(req, res) {
     <body>`);
     res.write('<h1>Autor</h1>');
     res.write('<p>Nome: George Paulo</p>');
-    res.write('<h2>FormaÃ§Ãµes acadÃªmicas:</h2>');
+    res.write('<h2>Formação acadêmica:</h2>');
     res.write('<ul>');
-    res.write('<li>TÃ©cnico em InformÃ¡tica para Internet</li>');
-    res.write('<li>Instituto Federal do CearÃ¡</li>');
+    res.write('<li>Técnico em Informática para Internet</li>');
+    res.write('<li>Instituto Federal do Ceará</li>');
     res.write('<li>Ano: 2023</li>');
     res.write('</ul>');
-    res.write('<h2>ExperiÃªncias profissionais:</h2>');
+    res.write('<h2>Experiências profissionais:</h2>');
     res.write('<ul>');
-    res.write('<li>Militar do ExÃ©rcito Brasileiro</li>');
+    res.write('<li>Militar do Exército Brasileiro</li>');
     res.write('<li>Trabalhando atualmente com infraestrutura de TI</li>');
     res.write('<li>Ano: Desde 2018</li>');
     res.write('</ul>');
@@ -128,7 +128,7 @@ function naoEncontrado(req, res) {
     <meta charset="UTF-8">
     </head>
     <body>`);
-    res.write('<h1>NÃ£o encontrado!</h1>');
+    res.write('<h1>Não encontrado!</h1>');
     res.write('<footer>');
     res.write('<p>Desenvolvido Por George Paulo</p>');
     res.write('</footer>');
